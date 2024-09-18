d0d10d8a0b8d6b291e4065b8239c713c219d714a
Gleriston <xpanium@gmail.com>
2024-09-18 19:50:49 -0300

    add with Tags Swegger

--- Program.cs
+++ Program.cs
@@ -27,7 +27,7 @@ var app = builder.Build();
 #endregion
 
 #region Home
-app.MapGet("/", () => Results.Json(new Home()));
+app.MapGet("/", () => Results.Json(new Home())).WithTags("Home");
 #endregion
 
 #region Administradores
@@ -36,7 +36,7 @@ app.MapPost("/administradores/login",([FromBody] LoginDTO loginDTO, IAdministrad
     return Results.Ok("Login efetuado com sucesso!");
   else
     return Results.Unauthorized();
-});
+}).WithTags("Administradores");
 #endregion
 
 #region Veiculos
@@ -50,12 +50,12 @@ app.MapPost("/veiculos",([FromBody] VeiculoDTO veiculoDTO, IVeiculosServico veic
   veiculoServico.Incluir(veiculo);
 
   return Results.Created($"/veiculo/{veiculo.Id}", veiculo);
-});
+}).WithTags("Veiculos");
 
 app.MapGet("/veiculos",([FromQuery] int? pagina, IVeiculosServico veiculoServico) => {
   var veiculos = veiculoServico.Todos(pagina);
   return Results.Ok(veiculos);
-});
+}).WithTags("Veiculos");
 #endregion
 
 #region App
