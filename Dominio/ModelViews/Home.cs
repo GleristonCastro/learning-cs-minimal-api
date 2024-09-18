f5e0c6f907becda17cf23b0b3e23df903d23c526
Gleriston <xpanium@gmail.com>
2024-09-18 01:32:02 -0300

    add home response json

---
+++ Dominio/ModelViews/Home.cs
@@ -0,0 +1,8 @@
+namespace MinimalApi.Dominio.ModelViews
+{
+    public class Home
+    {
+        public string Mensagem { get => "Bem bindo a API de veiculos - Mininal API"; }
+        public string Doc { get => "/swagger"; }
+    }
+}
