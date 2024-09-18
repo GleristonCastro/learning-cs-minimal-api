2e99c1204cc7c5a8ce44856da59286edc71ad77a
Gleriston <xpanium@gmail.com>
2024-09-17 23:18:41 -0300

    add administrator interface

---
+++ Dominio/Interfaces/IAdministradorServico.cs
@@ -0,0 +1,9 @@
+using MinimalApi.Dominio.Entidades;
+using MinimalApi.DTOs;
+
+namespace MinimalApi.Dominio.interfaces;
+
+public interface IAdministradorServico
+{
+  Administrador? Login(LoginDTO loginDTO);
+};
