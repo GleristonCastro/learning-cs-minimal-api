4767f113d626eed6a9dc63b1f401fd1583f0e33b
Gleriston <xpanium@gmail.com>
2024-09-18 01:31:40 -0300

    add momain interface veiculo

---
+++ Dominio/Interfaces/IVeiculosServico.cs
@@ -0,0 +1,13 @@
+using MinimalApi.Dominio.Entidades;
+using MinimalApi.DTOs;
+
+namespace MinimalApi.Dominio.interfaces;
+
+public interface IVeiculosServico
+{
+  List<Veiculo> Todos(int pagina = 1, string? nome = null, string? marca = null);
+  Veiculo? BuscarPorId(int id);
+  void Incluir(Veiculo veiculo);
+  void Atualizar(Veiculo veiculo);
+  void Apagar(int id);
+}
