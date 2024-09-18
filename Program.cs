5ed38c06bbc300ebe1a8b6198bbffdaf21aa6b56
Gleriston <xpanium@gmail.com>
2024-09-17 23:17:17 -0300

    add main program file in the project

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using MinimalApi.Infraestrutura.Db;
+using MinimalApi.DTOs;
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.interfaces;
+using MinimalApi.Dominio.Servicos;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
+
+builder.Services.AddDbContext<DbContexto>(
+  options => options.UseMySql(
+    builder.Configuration.GetConnectionString("mysql")?.ToString(),
+    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql")?.ToString())
+  )
+);
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/login",([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) => {
+  if (administradorServico.Login(loginDTO) != null)
+    return Results.Ok("Login efetuado com sucesso!");
+  else
+    return Results.Unauthorized();
+});
+
+app.Run();
