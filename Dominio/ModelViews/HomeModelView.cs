c83621eb44dfab80a97ecf90af59975d9759feeb
Gleriston <xpanium@gmail.com>
2024-09-22 00:04:01 -0300

    Rename file and class name

--- Dominio/ModelViews/HomeModelView.cs
+++ Dominio/ModelViews/HomeModelView.cs
@@ -1,8 +1,6 @@
-namespace MinimalApi.Dominio.ModelViews
-{
-    public class Home
+namespace MinimalApi.Dominio.ModelViews;
+    public class HomeModelView
     {
         public string Mensagem { get => "Bem bindo a API de veiculos - Mininal API"; }
         public string Doc { get => "/swagger"; }
     }
-}
