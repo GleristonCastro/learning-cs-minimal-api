915b259c7a9abe6aa4c20779808b1f7b180931d8
Gleriston <xpanium@gmail.com>
2024-09-22 00:05:06 -0300

    update administrador servico

--- Dominio/Servicos/AdministradorServico.cs
+++ Dominio/Servicos/AdministradorServico.cs
@@ -1,5 +1,5 @@
 using MinimalApi.Dominio.Entidades;
-using MinimalApi.Dominio.interfaces;
+using MinimalApi.Dominio.Interfaces;
 using MinimalApi.DTOs;
 using MinimalApi.Infraestrutura.Db;
 
@@ -15,9 +15,35 @@ public class AdministradorServico : IAdministradorServico
     _contexto = contexto;
   }
 
-  public Administrador? Login(LoginDTO loginDTO)
+    public Administrador? BuscarPorId(int id)
+    {
+      return _contexto.Administradores.Where(a => a.Id == id).FirstOrDefault();
+    }
+
+    public Administrador Incluir(Administrador administrador)
+    {
+    _contexto.Administradores.Add(administrador);
+    _contexto.SaveChanges();
+    return administrador;
+    }
+
+    public Administrador? Login(LoginDTO loginDTO)
   {
     var adm = _contexto.Administradores.Where(a => a.Email == loginDTO.Email && a.Senha == loginDTO.Senha).FirstOrDefault();
     return adm;
   }
+
+    public List<Administrador> Todos(int? pagina)
+    {
+      var query = _contexto.Administradores.AsQueryable();
+
+      int itensPorPagina = 10;
+
+      if(pagina != null)
+        query = query
+        .Skip(((int)pagina - 1) * itensPorPagina)
+        .Take(itensPorPagina);
+
+      return query.ToList();
+    }
 }
